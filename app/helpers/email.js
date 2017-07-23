ffd6519790c76c5cce1ef1855f78f6c09b5d371a
Adrian Dickin <adrian.dickin@gmail.com>
2017-07-23 13:14:05 +0100

    newsletter & email tweaks

--- app/helpers/email.js
+++ app/helpers/email.js
@@ -1,12 +1,20 @@
 module.exports.register = function(Handlebars) {
   'use strict';
 
-  Handlebars.registerHelper('email', function(emailAddr) {
-	var encodeEmail = emailAddr.replace('@', '\xAB').replace('a', '\xA9').replace('e', '\xAA').replace('.', '\xAC');
+  function doEmail(emailAddr, msg) {
+    var encodeEmail = emailAddr.replace('@', '\xAB').replace('a', '\xA9').replace('e', '\xAA').replace('.', '\xAC');
     var parts = emailAddr.split('@');
     var emailBody = '<span>'+parts[0] + '</span>&#64;<span>' + parts[1] + '</span>';
-    var safeEmail = '<a href="" class="brn btn-primary btn-xs emailLink" data-addr="' + encodeEmail + '">email</a>';
+    var safeEmail = '<a href="" class="brn btn-primary btn-xs emailLink" data-addr="' + encodeEmail + '">' + msg + '</a>';
     return safeEmail;
+  }
+
+  Handlebars.registerHelper('email', function(emailAddr) {
+    return doEmail(emailAddr, 'email');
+  });
+
+  Handlebars.registerHelper('emailWithLabel', function(emailAddr, label) {
+    return doEmail(emailAddr, label);
   });
 
   Handlebars.registerHelper('anchor', function(link) {
